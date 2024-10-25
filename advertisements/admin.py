d3ab746e975b5f7c5cf75c171132e09adcb5f849
Dushenko <Dushenko@broccolidev.ru>
2024-10-26 03:32:41 +0700

    test

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from advertisements.models import Advertisement
+
+admin.site.register(Advertisement)
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,15 +1,21 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
-        return []
+        return super().get_permissions()
