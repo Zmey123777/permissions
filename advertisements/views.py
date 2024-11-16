a0140476682f3d49a9536f9876ead16120402e59
Dushenko <Dushenko@broccolidev.ru>
2024-11-16 11:31:20 +0700

    fix patch and delete permissions

--- advertisements/views.py
+++ advertisements/views.py
@@ -5,6 +5,7 @@ from advertisements.serializers import AdvertisementSerializer
 from advertisements.filters import AdvertisementFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.exceptions import PermissionDenied
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -19,3 +20,16 @@ class AdvertisementViewSet(ModelViewSet):
         if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
         return super().get_permissions()
+    
+    def perform_update(self, serializer):
+        """Переопределение метода обновления."""
+        advertisement = self.get_object()
+        if advertisement.creator != self.request.user:
+            raise PermissionDenied("Вы не можете обновить это объявление.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        """Переопределение метода удаления."""
+        if instance.creator != self.request.user:
+            raise PermissionDenied("Вы не можете удалить это объявление.")
+        instance.delete()
